ea6f9a1ed62b695d307aebcf3a4955163579f149
Moira Leon <moira.leon2@gmail.com>
2021-09-02 11:30:00 -0600

    naming notes

---
+++ src/com/devmountain/java/mandrcode/Client.java
@@ -0,0 +1,18 @@
+package com.devmountain.java.mandrcode;
+
+public class Client {
+
+    public static void main(String[] args) {
+	System.out.println("Writing readable and maintainable code");
+    }
+}
+//separating and having more small classes with single responsibilities is best,
+//best options can be to split into smaller classes or to rename classes
+//keep class names short, simple, and basic
+//ideally 1-2 words
+//booleans should be prefaced with "is" ie isActive isValid
+//camel case, or all caps with _underscores for constants
+//never name variables with single letter ie d vs data
+//for methods name should give insight on intent and functionality
+//avoid abbreviations
+//avoid and or if in method names, if present in methods they should be broken into separate methods
